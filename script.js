56e83fbcc64e65d5b86d1ba9918b9bff0874be77
Anish Kumar <anish2272002@hotmail.com>
2022-07-08 08:14:57 +0530

    Landing and Why Join

---
+++ script.js
@@ -0,0 +1,43 @@
+AOS.init();
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jul 30, 2022 14:30:20").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "CLOSED";
+  }
+}, 1000);
+
+var myNav = document.getElementById('mynav');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
