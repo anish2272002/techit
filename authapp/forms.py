c41131935d2360bdba20be8e537362ebd5768597
Anish Kumar <anish2272002@hotmail.com>
2022-12-04 12:03:14 +0530

    Login

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,1 +1,21 @@
 from django import forms
+from django.contrib.auth import authenticate
+
+class LoginForm(forms.Form):
+    username=forms.CharField(
+        max_length=256,
+        label='',
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'})
+    )
+    password=forms.CharField(
+        max_length=256,
+        label='',
+        widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'})
+    )
+    def clean(self):
+        super().clean()
+        usr=authenticate(username=self.cleaned_data['username'],password=self.cleaned_data['password'])
+        if(usr):
+            self.cleaned_data['user']=usr
+        else:
+            raise forms.ValidationError("Invalid Credentials")
--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout
+from django.shortcuts import get_object_or_404
+
+from .forms import LoginForm
 
 class LoginView(View):
-    def get(self,request):
-        return render(request,"login.html",{})
+    form_class = LoginForm
+    template_name = 'login.html'
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            usr=form.cleaned_data['user']
+            login(request,usr)
+            return HttpResponseRedirect(reverse('main:index'))
+        return render(request, self.template_name, {'form': form})
+
+class LogoutView(View):
+    @method_decorator(login_required)
+    def get(self,request,*args,**kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse('main:index'))
