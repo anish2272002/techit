cf816dab2bc5c68d00709566b0bd77801f625772
Anish Kumar <anish2272002@hotmail.com>
2022-12-10 20:06:23 +0530

    Postgress DB

--- techit/settings.py
+++ techit/settings.py
@@ -90,12 +90,17 @@ WSGI_APPLICATION = 'techit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import dj_database_url
+DATABASE_URL = os.getenv("database_url")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
