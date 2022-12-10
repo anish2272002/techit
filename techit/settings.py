bb778ce9e9cb3c09482e92d713818c43ef4d5c96
Anish Kumar <anish2272002@hotmail.com>
2022-12-10 19:04:50 +0530

    CSRF_ORIGIN

--- techit/settings.py
+++ techit/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+ALLOWED_HOSTS = ["https://web-production-6ae1.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-6ae1.up.railway.app"]
 
 # Application definition
 
